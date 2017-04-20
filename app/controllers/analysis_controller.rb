0c7410d5522843f701233ee04c4e89f38ec7e023
cgsimmons <chris.simmons.alt@gmail.com>
2017-04-20 14:19:44 -0700

    Add search to analysis model

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -7,31 +7,7 @@ class AnalysisController < ApplicationController
   def create
     @analysis = Analysis.new analysis_params
     if @analysis.valid?
-      reddits = RedditService.new(
-        subreddit: @analysis.subreddit,
-        min_upvotes: @analysis.min_upvotes
-      ).listings
-      # byebug
-      # puts reddits
-      reddits.each do |post|
-        data = post['data']
-        subreddit_params = { name: data['subreddit'],
-                             reddit_id: data['subreddit_id'] }
-        post_params = { dump: post,
-                        ups: data['ups'],
-                        downs: data['downs'],
-                        reddit_id: data['name'],
-                        post_date_ts: data['created_utc'] }
-        sub = Subreddit.find_by(reddit_id: subreddit_params[:reddit_id])
-        sub = Subreddit.new(subreddit_params) if sub.nil?
-        # sub.save
-        new_post = Post.new(post_params)
-        new_post.subreddit = sub
-        new_post.save
-        # post_params['subreddit'] = s.id
-        # post_test = sub.posts.create(post_params)
-        # p.save
-      end
+      @analysis.search
     else
       flash.now[:alert] = 'Please complete the form below.'
     end
--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -29,4 +29,29 @@ class Analysis
   def persisted?
     false
   end
+
+  def search
+    posts = RedditService.new(
+      subreddit: @subreddit,
+      min_upvotes: @min_upvotes
+    ).listings
+    # byebug
+    # puts reddits
+    posts.each do |post|
+      data = post['data']
+      subreddit_params = { name: data['subreddit'],
+                           reddit_id: data['subreddit_id'] }
+      post_params = { dump: post,
+                      ups: data['ups'],
+                      downs: data['downs'],
+                      reddit_id: data['name'],
+                      post_date_ts: data['created_utc'] }
+      sub = Subreddit.find_by(reddit_id: subreddit_params[:reddit_id])
+      sub = Subreddit.new(subreddit_params) if sub.nil?
+      # sub.save
+      new_post = Post.new(post_params)
+      new_post.subreddit = sub
+      new_post.save
+    end
+  end
 end
